0bb151e81155b78e82f43436cb092f1771a67508
Yongze <yongze1997@gmail.com>
2024-12-01 16:44:13 +0800

    Book Us page

--- script.js
+++ script.js
@@ -20,3 +20,37 @@ prevButton.addEventListener('click', () => {
   updateCarousel();
 });
 
+let btnSubmit = document.getElementById('submit-button');
+    let msg = "";
+    //process form 
+    btnSubmit.addEventListener('click', function (e) {
+        e.preventDefault();
+  
+        //get first-name value
+        let FirstName = document.getElementById('first-name').value;
+        msg += `First Name: ${FirstName}<br/>`;
+
+        if(FirstName.length < 1){
+            errorMsg = `Please enter your first name.`;
+          console.log(errorMsg);
+              document.getElementById('errorUsername').innerHTML = errorMsg;
+        }else{
+          document.getElementById('errorUsername').innerHTML = "";
+        }
+        
+        //get Last-name value
+        let Lastname = document.getElementById('last-name').value;
+        msg += `Last Name: ${Lastname}<br/>`;
+
+        //get Contactno value
+        let contactno = document.getElementById('contactno').value;
+        msg += `Contact No: ${contactno}<br/>`;
+
+        //get datetime value
+        let datetime = document.getElementById('datetime').value;
+        msg += `Date & Time: ${datetime}<br/>`;
+
+        //get message value
+        let message = document.getElementById('message-id').value;
+        msg += `Message: ${message}<br/>`;
+    })
